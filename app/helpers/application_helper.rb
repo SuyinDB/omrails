342a3fbcb1936da1ef2c72ee003ecf960244c126
Suyin Du Bois <suyin.dubois@gmail.com>
2013-11-12 18:12:15 +0000

    Added talking to myself

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	def other_conversation_people(conversation)
+		other_people = conversation.messages.reject { |m| m.sender == current_user }
+		if other_people.any?
+			other_people.map { |m| m.sender.name }.uniq.to_sentence
+		else
+			"myself"
+		end
+	end
+
 end
