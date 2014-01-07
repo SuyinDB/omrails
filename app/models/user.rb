00921ce938e87b6199d9af1c0615c08e9c43f10c
Suyin Du Bois <suyin.dubois@gmail.com>
2014-01-07 22:42:24 +0000

    Added profile pictures

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ActiveRecord::Base
   validates :name, :username, :email, :password, :password_confirmation, presence: true
   validates :username, uniqueness: { case_sensitive: false }
 
+  has_attached_file :avatar, styles: { medium: "320x240>"}
+  validates_attachment :avatar, content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
+                               size: { less_than: 5.megabytes }
+
 	acts_as_messageable
 
   # Include default devise modules. Others available are:
@@ -11,7 +15,7 @@ class User < ActiveRecord::Base
          :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :username, :name, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :username, :name, :email, :avatar, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
 
   has_many :pins, :dependent => :destroy
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131110131747) do
+ActiveRecord::Schema.define(:version => 20140107162808) do
 
   create_table "conversations", :force => true do |t|
     t.string   "subject",    :default => ""
@@ -80,8 +80,12 @@ ActiveRecord::Schema.define(:version => 20131110131747) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
-    t.string   "name"                                
-    t.string   "username"                                                        
+    t.string   "name"
+    t.string   "username"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
